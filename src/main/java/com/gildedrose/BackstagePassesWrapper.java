f536ac0ca2213eeebdd5207ff3e3f5ce710937a6
9elmaz9 <@.com>
2024-10-06 21:15:03 +0200

    Replaced magic numbers with constants in BackstagePassesWrapper

--- src/main/java/com/gildedrose/BackstagePassesWrapper.java
+++ src/main/java/com/gildedrose/BackstagePassesWrapper.java
@@ -2,28 +2,32 @@ package com.gildedrose;
 
 public class BackstagePassesWrapper extends ItemWrapper {
 
+    private static final int MAX_QUALITY = 50;
+    private static final int NEAR_CONCERT_DAYS = 10;
+    private static final int VERY_CLOSE_CONCERT_DAYS = 5;
+
     public BackstagePassesWrapper(Item item) {
         super(item);
     }
 
     @Override
     public void updateQuality() {
-        if (item.quality < 50) {
+        if (item.quality < MAX_QUALITY) {
             item.quality++;
-
-            if (item.sellIn <= 10 && item.quality < 50) {
-                item.quality++;
-            }
-
-            if (item.sellIn <= 5 && item.quality < 50) {
-                item.quality++;
-            }
         }
+
+        if (item.sellIn <= NEAR_CONCERT_DAYS && item.quality < MAX_QUALITY) {
+            item.quality++;
+        }
+
+        if (item.sellIn <= VERY_CLOSE_CONCERT_DAYS && item.quality < MAX_QUALITY) {
+            item.quality++;
+        }
+
         item.quality--;
 
         if (item.sellIn < 0) {
             item.quality = 0;
         }
-
     }
 }
